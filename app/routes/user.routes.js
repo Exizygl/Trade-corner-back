ff7808f1651cbea2c4b80bfd98e069b0c9a5d681
Jordan <jordanmahaut09@gmail.com>
2022-06-11 23:25:49 +0200

    Création des routes pour la direction au login

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -2,4 +2,12 @@ const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
 const userController = require("../controllers/user.controller");
 
+// Router POST
+
 router.post("/register", userController.signUp);
+router.post("/login", userController.signIn);
+
+// Router GET
+
+router.get("/", userController.getAllUsers);
+// Router put
