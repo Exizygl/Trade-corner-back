9da2556e802ad414902af3db0a79ace43816f475
Jordan <jordanmahaut09@gmail.com>
2022-06-11 19:41:55 +0200

    DB Config - Connexion au serveur

--- app/config/server.config.js
+++ app/config/server.config.js
@@ -1,5 +1,22 @@
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 module.exports = {
-    PORT: process.env.SERVER_PORT,
-}
+  PORT: process.env.SERVER_PORT,
+};
+
+mongoose.connect(
+  "mongodb+srv://" +
+    process.env.DB_USER_PASS +
+    "@cluster0.kaa7z.mongodb.net/E-Commerce",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+  (err) => {
+    if (!err) console.log("Mongodb connected");
+    else console.log("Connection error :" + err);
+  }
+);
--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const express = require('express')
-const serverConfig = require('./app/config/server.config')
-const bodyParser = require('body-parser');
+const express = require("express");
+const serverConfig = require("./app/config/server.config");
+const bodyParser = require("body-parser");
+require("./app/config/server.config");
 
-const PORT = serverConfig.PORT || 5000
+const PORT = serverConfig.PORT || 5000;
 
-const app = express()
+const app = express();
 
 app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  
-  });
-  
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
-  
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 //Routes
-app.get('/api',(req,res) => res.status(200).send({message : 'test server'}))
+app.get("/api", (req, res) => res.status(200).send({ message: "test server" }));
 
-
-
-app.listen(PORT,  () => console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
