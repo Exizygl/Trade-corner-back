198fa32edaf188c665117166a5b5da933c4c0259
JordanMahaut <jordanmahaut09@gmail.com>
2022-06-20 14:58:17 +0200

    Transfert de fichier

---
+++ app/controllers/Auth/auth.controller.js
@@ -0,0 +1,54 @@
+const UserModel = require("../../models/user.model");
+const jwt = require("jsonwebtoken");
+
+const maxAge = 3 * 24 * 60 * 60 * 1000;
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.TOKEN_SECRET, {
+    expiresIn: 3 * 24 * 60 * 60 * 1000,
+  });
+};
+
+module.exports.signUp = async (req, res) => {
+  console.log("signUp")
+  const { 
+    pseudo,
+    name,
+    email,
+    phoneNumber,
+    adress,
+    zipcode,
+    ville,
+    password,
+    passwordConfirmation,
+    cgu, } = req.body;
+
+  try {
+    const user = await UserModel.create({ 
+      pseudo,
+      name,
+      email,
+      phoneNumber,
+      adress,
+      zipcode,
+      ville,
+      password,
+      passwordConfirmation,
+      cgu, });
+    res.status(201).json({ user: user._id });
+  } catch (err) {
+    res.status(400).send({ err });
+  }
+};
+
+module.exports.signIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await UserModel.login(email, password);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge });
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    res.status(200).json(err);
+  }
+};
