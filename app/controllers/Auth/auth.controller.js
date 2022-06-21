437861c08315044f345ebc402f22dc797d88a612
JordanMahaut <jordanmahaut09@gmail.com>
2022-06-21 13:39:34 +0200

    Ajout du module Cors

--- app/controllers/Auth/auth.controller.js
+++ app/controllers/Auth/auth.controller.js
@@ -9,8 +9,8 @@ const createToken = (id) => {
 };
 
 module.exports.signUp = async (req, res) => {
-  console.log("signUp")
-  const { 
+  console.log("signUp");
+  const {
     pseudo,
     name,
     email,
@@ -20,11 +20,11 @@ module.exports.signUp = async (req, res) => {
     ville,
     password,
     passwordConfirmation,
-    cgu, 
+    cgu,
   } = req.body;
 
   try {
-    const user = await UserModel.create({ 
+    const user = await UserModel.create({
       pseudo,
       name,
       email,
@@ -34,7 +34,8 @@ module.exports.signUp = async (req, res) => {
       ville,
       password,
       passwordConfirmation,
-      cgu, });
+      cgu,
+    });
     res.status(201).json({ user: user._id });
   } catch (err) {
     res.status(400).send({ err });
@@ -42,11 +43,11 @@ module.exports.signUp = async (req, res) => {
 };
 
 module.exports.signIn = async (req, res) => {
-  console.log("signIn")
+  console.log("signIn");
   const { email, password } = req.body;
 
   try {
-    const user = await UserModel.login(email, password);
+    const user = await UserModel.create(email, password);
     const token = createToken(user._id);
     res.cookie("jwt", token, { httpOnly: true, maxAge });
     res.status(200).json({ user: user._id });
