32b3df8fc63c0710de36bff33ee28b6adda31326
JordanMahaut <jordanmahaut09@gmail.com>
2022-06-22 13:39:32 +0200

    Fonction expiration des TOKEN JWT

--- app/controllers/Auth/auth.controller.js
+++ app/controllers/Auth/auth.controller.js
@@ -74,7 +74,23 @@ function validateEmail(email) {
   return re.test(email);
 }
 
-const createActivationToken = (payload) => {};
+const createActivationToken = (payload) => {
+  return jwt.sign(payload, proccess.env.ACTIVATION_TOKEN_SECRET, {
+    expiresIn: "5m",
+  });
+};
+
+const createAccessToken = (payload) => {
+  return jwt.sign(payload, proccess.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
+};
+
+const createRefreshToken = (payload) => {
+  return jwt.sign(payload, proccess.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+};
 
 // module.exports.signIn = async (req, res) => {
 //   console.log("signIn");
