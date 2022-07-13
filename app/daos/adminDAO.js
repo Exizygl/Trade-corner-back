8522e2d69e4fe6b7376d858b58da4178ab307ce9
sara-meunier <sara.meunie@gmail.com>
2022-07-13 18:35:05 +0200

    essai de gestion des erreurs + ajout random email

--- app/daos/adminDAO.js
+++ app/daos/adminDAO.js
@@ -1,9 +1,21 @@
 const UserModel = require("../models/user.model");
 const ObjectId = require("mongoose").Types.ObjectId;
 
+//generateur d'email random
+function getRandomEmail (domain, length) {
+  var chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+  var string = '';
+  for(var i=0; i<length; i++){
+      string += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return string + domain
+}
+
 const getById = async (id) => await UserModel.findOne({ _id: id });
 
-const deleteUser = async (userId) =>
+const deleteUser = async (userId) => {
+
+var email = getRandomEmail("@delete.com", 14);
   await UserModel.updateOne({ _id: ObjectId(userId) }, {Avatar: "supprimé", 
                                                         pseudo:"supprimé", 
                                                         name: "supprimé", 
@@ -11,8 +23,10 @@ const deleteUser = async (userId) =>
                                                         adress:"supprimé", 
                                                         zipcode:"supprimé", 
                                                         ville:"supprimé", 
-                                                        password:"supprimé"
+                                                        password:"supprimé",
+                                                        email: email,
   });
+}
 
   module.exports = {
     getById,
--- app/services/adminService.js
+++ app/services/adminService.js
@@ -2,26 +2,33 @@ const adminDAO = require("../daos/adminDAO");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+
+
 const deleteUser = async (req,res,next) => {
     const adminPassword = req.password;
     try {
-    console.log("attention du back : " + JSON.stringify(req));
-    adminDAO.getById(req.userId)
-    .then (async (user) => {
-        if (!user) {
-            return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-          }
-        const isMatch = await user.comparePassword(adminPassword);
-        if (!isMatch) throw "Authentication error - wrong password";
-        return await adminDAO.deleteUser(req.userToDeleteId);
-    })
-    
-  } catch (err) {
+        console.log("attention du back : " + JSON.stringify(req));
+        adminDAO.getById(req.userId)
+        .then (async (user) => {
+            if (!user) {
+                return res.status(401).json({ error: 'Utilisateur non trouvé !' });}
+            const isMatch = await user.comparePassword(adminPassword);
+            if (!isMatch) {
+                throw "Authentication error - wrong password"}
+            //     return res.status(500).json({ msg: error })}
+            else {
+                return await adminDAO.deleteUser(req.userToDeleteId);
+            }
+        })
+        .catch ((error) => {
+            console.log("une erreur est survenue : " + error);
+            // throw error;
+            // return res.status(500).json({ msg: error });
+        })
+    } catch (err) {
     console.log(err);
-    return res.status(500).json({ msg: err.message });
+    return res.status(500).json({ msg: err });
   }
-
-
 };
 
 module.exports = {
