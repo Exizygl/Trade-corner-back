1dd34c0e947e441da7edc499b079e21dae1c8075
Exizygl <exizelyglaya@gmail.com>
2022-09-21 11:54:36 +0200

    problem

--- app/routes/product.routes.js
+++ app/routes/product.routes.js
@@ -2,24 +2,16 @@ const router = require('express').Router();
 const ProductService = require('../services/productService');
 const { hasJWT } = require('../middlewares/jwt');
 
-<<<<<<< HEAD
 const successCbk = require("../misc/callbacks").successCbk;
 const errorCbk = require("../misc/callbacks").errorCbk;
 const uploadProductPhotos = require("../middlewares/uploadProductPhotos");
 const upload = require("../middlewares/upload");
 const { successCbkProduct } = require("../misc/callbacks");
-=======
-const successCbk = require('../misc/callbacks').successCbk;
-const errorCbk = require('../misc/callbacks').errorCbk;
-const uploadProductPhotos = require('../middlewares/uploadProductPhotos');
-const upload = require('../middlewares/upload');
->>>>>>> origin/getProductsFrom
 
 router.post('/add', hasJWT, uploadProductPhotos, async (req, res) => {
   console.log('ça rentre dans la route : ');
 
   try {
-<<<<<<< HEAD
       const product = await ProductService.addProduct(req.files, req.body, req.userId);// add product to schema
       
       // const image = await ProductService.uploadImageUser(
@@ -45,24 +37,6 @@ router.put("/modify", hasJWT, uploadProductPhotos, async (req, res) => {
       }
     });  
 
-=======
-    const product = await ProductService.addProduct(
-      req.files,
-      req.body,
-      req.userId
-    ); // add product to schema
-
-    // const image = await ProductService.uploadImageUser(
-    //   req.file ? req.file.filename : "",
-    //   product._id
-    // );
-
-    return successCbk(res, 200, { product });
-  } catch (error) {
-    return res.status(201).send(error);
-  }
-});
->>>>>>> origin/getProductsFrom
 // Router GET
 
 router.get('/', async (req, res) => {
@@ -93,7 +67,6 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-<<<<<<< HEAD
 router.get("/search/:search/:page/:superCategory/:category/:order/:minimun/:maximun", async (req , res) => {
   try {
     
@@ -115,34 +88,6 @@ router.get("/search/:search/:page/:superCategory/:category/:order/:minimun/:maxi
   }
 });
 
-=======
-router.get(
-  '/search/:search/:page/:superCategory/:category',
-  async (req, res) => {
-    try {
-      console.log('toya');
-      const productList = await ProductService.search(req.params);
-      console.log(productList);
-
-      return successCbk(res, 200, { productList });
-    } catch (error) {
-      return res.status(201).send({ error });
-    }
-  }
-);
-router.get(
-  '/searchCount/:search/:superCategory/:category',
-  async (req, res) => {
-    try {
-      const number = await ProductService.searchCount(req.params);
-
-      return successCbk(res, 200, { number });
-    } catch (error) {
-      return res.status(201).send({ error });
-    }
-  }
-);
-
 router.get('/user/:id', async (req, res) => {
   try {
     const productList = await ProductService.getProductsFrom(req.params.id);
@@ -152,6 +97,5 @@ router.get('/user/:id', async (req, res) => {
     return res.status(201).send({ error });
   }
 });
->>>>>>> origin/getProductsFrom
 
 module.exports = router;
