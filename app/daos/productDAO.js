036f337320fff55033c9d4b9da3baa786892a316
Exizygl <exizelyglaya@gmail.com>
2022-09-21 11:31:06 +0200

    merge

--- app/daos/productDAO.js
+++ app/daos/productDAO.js
@@ -13,12 +13,15 @@ const getAllProducts = async () =>
     .populate('tagIdList', 'tag')
     .populate('categoryId', 'label');
 
-<<<<<<< HEAD
 const getNewProducts = async () => await ProductModel.find().sort({ createdAt: -1 }).limit(4).populate('tagIdList', 'tag').populate('categoryId', 'label');
 
 const getByTitle = async (label) =>
     await ProductModel.find({ title: new RegExp(label, "i") })
-
+    
+const getProductsFrom = async (id) =>
+    await ProductModel.find({ sellerId: id })
+      .populate('tagIdList', 'tag')
+      .populate('categoryId', 'label');
 
 const searchCategory = async (search, tagIdList, categoryIdList, orderType, orderValue, minimun, maximun) => await ProductModel.find({
   $or :[
@@ -38,83 +41,21 @@ const search = async (search, tagIdList,  orderType, orderValue, minimun, maximu
   ],
     "price": {"$gte": minimun, "$lte": maximun }})
     .sort({[orderType]: orderValue})
-=======
-const getNewProducts = async () =>
-  await ProductModel.find()
-    .sort({ createdAt: -1 })
-    .limit(4)
-    .populate('tagIdList', 'tag')
-    .populate('categoryId', 'label');
-
-const getProductsFrom = async (id) =>
-  await ProductModel.find({ sellerId: id })
-    .populate('tagIdList', 'tag')
-    .populate('categoryId', 'label');
-
-const searchPaginationCategory = async (search, page, limit, IdList) =>
-  await ProductModel.find({
-    title: {
-      $regex: search,
-      $options: 'i',
-    },
-    categoryId: { $in: IdList },
-  })
-    .skip(page * limit)
-    .limit(limit)
->>>>>>> origin/getProductsFrom
     .populate('tagIdList', 'tag')
     .populate('categoryId', 'label')
     .populate('sellerId', 'pseudo');
 
-<<<<<<< HEAD
 
 
 
-=======
-const searchPagination = async (search, page, limit) =>
-  await ProductModel.find({
-    title: {
-      $regex: search,
-      $options: 'i',
-    },
-  })
-    .skip(page * limit)
-    .limit(limit)
-    .populate('tagIdList', 'tag')
-    .populate('categoryId', 'label')
-    .populate('sellerId', 'pseudo');
-
-const search = async (search) =>
-  await ProductModel.find({
-    title: {
-      $regex: search,
-      $options: 'i',
-    },
-  });
-const searchCategory = async (search, IdList) =>
-  await ProductModel.find({
-    title: {
-      $regex: search,
-      $options: 'i',
-    },
-    categoryId: { $in: IdList },
-  });
->>>>>>> origin/getProductsFrom
 
 module.exports = {
   addProduct,
   getById,
   getAllProducts,
   getNewProducts,
-<<<<<<< HEAD
   search,
   searchCategory,
-  getByTitle
-=======
-  searchPagination,
-  searchPaginationCategory,
-  search,
-  searchCategory,
+  getByTitle,
   getProductsFrom,
->>>>>>> origin/getProductsFrom
 };
